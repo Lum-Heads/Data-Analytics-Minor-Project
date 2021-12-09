191a47d3f32bcadeed1349e1f26602aa18f9b59f
Abhinav Dubey <dubey.abhinav76@gmail.com>
2021-12-09 11:31:41 +0530

    pagination implemented

--- src/Components/Home.component.jsx
+++ src/Components/Home.component.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import useGoogleSheets from "use-google-sheets";
 import Card from "./Card.component";
+import Pagination from "./Pagination";
 
 function Home() {
-  return (
-    <div className="container">
-      <h1 className="text-center p-3">Get your dream job now !</h1>
-      <Card />
-    </div>
-  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(12);
+
+  const {
+    data: response,
+    loading,
+    error,
+  } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  if (response) {
+    const jobInfo = response[0].data;
+    //Get current Posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = jobInfo.slice(indexOfFirstPost, indexOfLastPost);
+
+    //change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+      <div className="container">
+        <h1 className="text-center p-3">Get your dream job now !</h1>
+        <Card posts={currentPosts} />
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={jobInfo.length}
+          paginate={paginate}
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/Components/Navbar.component.jsx
+++ src/Components/Navbar.component.jsx
@@ -3,22 +3,11 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark p-3">
+      <nav className="navbar custom-background p-3">
         <div className="container-fluid">
-          <a href="/" className="navbar-brand">
+          <a href="/" className="navbar-brand text-white text-center">
             Freshify
           </a>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </nav>
     </div>
